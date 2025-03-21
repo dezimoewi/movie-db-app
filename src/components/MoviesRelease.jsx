c33395d40363099b8900eded3d7a994dd0f9e404
dezimoewi <dezimoewi@gmail.com>
2025-03-22 00:20:40 +0100

    new changes

--- src/components/MoviesRelease.jsx
+++ src/components/MoviesRelease.jsx
@@ -5,10 +5,9 @@ function MoviesRelease({movie}  ) {
     <div className="two" style={{cursor: 'pointer'}}>
 
         {/* <h2>{movie.original_title}</h2> */}
-      <img src={
+      <img src= {
         movie.poster_path
         ?`https://image.tmdb.org/t/p/w500${movie.poster_path}`: 'fallback_image_url'} alt={movie.original_title} />
-        <p className="title-paragraph">{movie.original_title}</p>
     </div>
   );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,55 +1,58 @@
 export default function Footer() {
-    return (
-      <>
-        <footer className="footer">
-          <div className="bfour">
-            <h1>Company</h1>
-            <p>About Us</p>
-            <p>Careers</p>
+  return (
+    <>
+      <div className="footer">
+        <div className="footer-top">
+          <div className="footer-comp">
+              <h3>Company</h3>
+              <p>About Us</p>
+            <p>Careers</p> 
+            
+            
           </div>
-  
-          <div className="bfive">
-            <h1>Need Help</h1>
+          <div className="footer-comp">
+            <h3>Need Help</h3>
             <p>Visit Help Center?</p>
             <p>Share Feedback</p>
           </div>
-  
-          <div className="footer-web">
-            <h1>Veiw Website in</h1>
+
+          <div className="footer-comp">
+            <h3>Veiw Website in</h3>
             <p>√ English ⩒</p>
           </div>
-  
-          <div className="bseven">
-            <h1>Social Media</h1>
+
+          <div className="footer-comp">
+            <h3>Social Media</h3>
             <div>
               <img src="" alt="" />
               <img src="" alt="" />
             </div>
           </div>
-  
-          <div className="beight">
-            <h1>Download Our App</h1>
+
+          <div className="footer-comp">
+            <h3>Download Our App</h3>
             <div>
               <img src="\src\assets\Rectangle 4 (1).png" alt="" /> <br />
               <img src="\src\assets\Rectangle 5.png" alt="" />
             </div>
           </div>
-  
-         
-        </footer>
-        <div className="footer-foot">
-            <p>©2023 STREAM X. All Rights Reserved.</p>
-            <div className="sd">
-              <a href="">Terms Of Use</a>
-              <a href="">Privacy Policy</a>
-              <a href="">FAQ</a>
-            </div>
-  
-            <div className="bone">
-              <h1>STREAM<span>X</span></h1>
-            </div>
+        </div>
+
+        <div className="footer-bottom">
+          <p>©2023 STREAM X. All Rights Reserved.</p>
+          <div className="footer-nav">
+            <a href="">Terms Of Use</a>
+            <a href="">Privacy Policy</a>
+            <a href="">FAQ</a>
           </div>
-      </>
-    );
-  }
-  
+
+          <div className="bone">
+            <h1>
+              STREAM<span>X</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
