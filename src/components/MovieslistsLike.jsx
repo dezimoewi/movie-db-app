b3062b92886325a182c38d1e425a1d5ecc757bc4
dezimoewi <dezimoewi@gmail.com>
2026-06-15 12:21:09 +0100

    review changes

--- src/components/MovieslistsLike.jsx
+++ src/components/MovieslistsLike.jsx
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
 
-function MovieslistsLike({movie}  ) {
+function MovieslistsLike({movie}) {
   return (
-    <div className="four"  style={{cursor: 'pointer'}}>
-
-        {/* <h2>{movie.original_title}</h2> */}
-      <img src= {
-        movie.poster_path
-        ?`https://image.tmdb.org/t/p/w500${movie.poster_path}`: 'fallback_image_url'} alt={movie.original_title} />
-        <p className="title-paragraph">{movie.original_title}</p>
+    <div className="movie-card" style={{cursor: 'pointer'}}>
+      <div className="movie-card-image">
+        <img src={
+          movie.poster_path
+          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'fallback_image_url'} alt={movie.original_title} />
+        <div className="movie-card-rating">
+          <span>⭐ {movie.vote_average?.toFixed(1)}</span>
+        </div>
+      </div>
+      <div className="movie-card-info">
+        <p className="movie-card-title">{movie.original_title}</p>
+        <div className="movie-card-meta">
+          <span className="movie-card-date">{movie.release_date?.split('-')[0]}</span>
+          <span className="movie-card-votes">♥ {movie.vote_count}</span>
+        </div>
+      </div>
     </div>
   );
 }
