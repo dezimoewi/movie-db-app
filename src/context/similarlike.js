9ce5a97e91f2a860ba1a17599c8b53ec8d2121db
dezimoewi <dezimoewi@gmail.com>
2026-06-16 12:13:17 +0100

    security: clean hardcoded TMDB keys and remove env tracking

--- src/context/similarlike.js
+++ src/context/similarlike.js
@@ -1,4 +1,4 @@
-const API_KEY ="7e6a1ec889d282a86311b6babd0a9b70"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 
 const BASE_MOVIE_URL =`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=6&sort_by=popularity.desc`;
@@ -9,7 +9,7 @@ export const fetchMovieLike= async() => {
     headers: {
       accept: "application/json",
       Authorization:
-        "7e6a1ec889d282a86311b6babd0a9b70",
+        `Bearer ${API_KEY}`,
     },
   };
     try {
